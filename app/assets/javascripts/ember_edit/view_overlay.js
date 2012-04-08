613e3bbf5a92243bef915802252a3842b5003c92
Anders Nøklestad <anders.noklestad@iln.uio.no>
2012-04-08 21:58:40 +0200

    Initial definition of ViewOverlay

---
+++ app/assets/javascripts/ember_edit/view_overlay.js
@@ -0,0 +1,12 @@
+window.EmberEdit = Ember.Namespace.create({
+
+  ViewOverlay: Ember.Object.extend({
+    // HTML element
+    element: null,
+
+    // The Ember view that this overlay will highlight
+    emberView: null
+  })
+
+});
+
