2f46a61af9de2bad1c10fbe3205abbc42e24cda5
Anders Nøklestad <anders.noklestad@iln.uio.no>
2012-05-16 22:50:29 +0200

    Add a few default settings

--- lib/tl_ember_edit.rb
+++ lib/tl_ember_edit.rb
@@ -1,4 +1,16 @@
 require "tl_ember_edit/engine"
 
 module TlEmberEdit
+
+  # Override these settings in an initializer or your application.rb
+
+  # The editor command. 'FILENAME' will be replaced by the actual file name.
+  # EDIT_CMD = "vim FILENAME"
+  # EDIT_CMD = "emacs FILENAME"
+  # EDIT_CMD = "mate FILENAME"
+  EDIT_CMD      = "subl FILENAME"
+
+  EMBER_ROOT    = "app/assets/javascripts"
+  VIEWS_DIR     = "views"
+
 end
